a42aba3c8b86fba44ac81ea5eb41a13d22283d9c
arun <arun.vijayan.pillai@shortcut.io>
2024-09-15 10:38:41 +0200

    adding dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,4 +15,7 @@ dependencies {
 
 kotlin {
     jvmToolchain(17)
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
--- score-board-app/build.gradle.kts
+++ score-board-app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
 }
+
 
 group = "net.insi8.scoreboard.app"
 version = "1.0-SNAPSHOT"
@@ -10,12 +14,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(project(":score-provider-lib"))
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+
+    implementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("io.projectreactor:reactor-test")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(17)
 }
